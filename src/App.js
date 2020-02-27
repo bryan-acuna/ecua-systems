238731094e1bbc88ca4c7646a568750550a94da8
Bryan Acuna <bryanacuna1994@icloud.com>
2020-02-27 18:24:12 -0500

    Adding all files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Homepage from './homepage.component'
+
+
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+        <Homepage/>
+      </div>
+    );
+  }
+}
+
+
+export default App;
